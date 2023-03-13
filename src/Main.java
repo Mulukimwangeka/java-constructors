5b18623561c1ee5f3904f373ef841373f83984a1
mulukimwangeka <mwangekaruth@gmail.com>
2023-03-13 08:37:30 +0300

    java constructors created

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    int modelyear;
+    String modelName;
+
+    public Main(int year, String name) {
+        modelyear = year;
+        modelName = name;
+    }
+
+    public static void main (string []args){
+        Main myCar = new Main(1969, "Mustang");
+        system.out.println(myCar.modelyear + " " + myCar.modelName
+    }
+
+
+
+}
